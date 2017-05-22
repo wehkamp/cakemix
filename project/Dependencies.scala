df53a5093446caf83734831a37c2f37b631bb36c
Sebastian Gavril <sgavril@wehkamp.nl>
2017-05-22 16:47:23 +0300

    Bump dependencies and plugins

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,15 +7,15 @@ object Dependencies {
     resolvers += Resolver.jcenterRepo
   )
 
-  val akkaVersion  = "2.4.9"
+  val akkaVersion  = "2.5.1"
 
   val akkaActor     = "com.typesafe.akka"  %% "akka-actor"       % akkaVersion
   val akkaSlf4j     = "com.typesafe.akka"  %% "akka-slf4j"       % akkaVersion
 
-  val ficus         = "com.iheart"         %% "ficus"            %  "1.2.6"
+  val ficus         = "com.iheart"         %% "ficus"            %  "1.4.0"
 
   val akkaTestkit   = "com.typesafe.akka"  %% "akka-testkit"     % akkaVersion
-  val scalatest     = "org.scalatest"      %% "scalatest"        % "3.0.0"
+  val scalatest     = "org.scalatest"      %% "scalatest"        % "3.0.1"
 
   def compile   (deps: ModuleID*): Seq[ModuleID] = deps map (_ % "compile")
   def provided  (deps: ModuleID*): Seq[ModuleID] = deps map (_ % "provided")
