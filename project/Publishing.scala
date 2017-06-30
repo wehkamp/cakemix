9705f620ef41ee1671f3dcf83cf586afe71ff984
Arnout Engelen <arnouten@bzzt.net>
2017-06-30 15:19:05 +0200

    Fix references to stamina in the generated pom.xml

--- project/Publishing.scala
+++ project/Publishing.scala
@@ -22,10 +22,10 @@ object Publishing {
         password <- sys.env.get("SONATYPE_PASSWORD")
       } yield Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", username, password)).toSeq,
     pomExtra := (
-  <url>https://github.com/scalapenos/stamina#readme</url>
+  <url>https://github.com/xebia/cakemix#readme</url>
   <scm>
-    <url>git@github.com:scalapenos/stamina.git</url>
-    <connection>scm:git@github.com:scalapenos/stamina.git</connection>
+    <url>git@github.com:xebia/cakemix.git</url>
+    <connection>scm:git@github.com:xebia/cakemix.git</connection>
   </scm>
   <developers>
     <developer>
