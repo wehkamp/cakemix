266d2c6f03205359d2585760e5f7be6f438ca927
Philippus Baalman <philippus@gmail.com>
2017-07-03 13:52:06 +0200

    Add settings for cross-releasing 2.11.x and 2.12.x versions

--- project/Build.scala
+++ project/Build.scala
@@ -9,6 +9,7 @@ object Build extends Build {
   lazy val basicSettings = Seq(
     organization := "com.xebia",
     scalaVersion := "2.12.2",
+    crossScalaVersions := Seq("2.11.11", "2.12.2"),
     scalacOptions := basicScalacOptions,
     incOptions := incOptions.value.withNameHashing(true),
     licenses := Seq("The MIT License (MIT)" -> url("http://opensource.org/licenses/MIT"))
