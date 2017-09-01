d79e703b6e5cdb2744a37f725105a2b4478ea414
Philippus Baalman <philippus@gmail.com>
2017-09-01 17:05:54 +0200

    Update to Scala 2.12.3

--- project/Build.scala
+++ project/Build.scala
@@ -7,8 +7,8 @@ object Build extends Build {
 
   lazy val basicSettings = Seq(
     organization := "com.xebia",
-    scalaVersion := "2.12.2",
-    crossScalaVersions := Seq("2.11.11", "2.12.2"),
+    scalaVersion := "2.12.3",
+    crossScalaVersions := Seq("2.11.11", "2.12.3"),
     scalacOptions := basicScalacOptions,
     incOptions := incOptions.value.withNameHashing(true),
     licenses := Seq("The MIT License (MIT)" -> url("http://opensource.org/licenses/MIT"))
