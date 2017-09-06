8f52d2849021b114c5b4d57cc64e7a533a645462
Philippus Baalman <philippus@gmail.com>
2017-09-06 10:38:55 +0200

    Adopt scalariform 1.8.0 style

--- project/Formatting.scala
+++ project/Formatting.scala
@@ -1,20 +1,12 @@
-import sbt._
-import com.typesafe.sbt.SbtScalariform
 import com.typesafe.sbt.SbtScalariform.ScalariformKeys
 import scalariform.formatter.preferences._
 
 object Formatting {
-  lazy val formattingSettings = SbtScalariform.scalariformSettings ++ Seq(
-    ScalariformKeys.preferences in Compile := formattingPreferences,
-    ScalariformKeys.preferences in Test    := formattingPreferences
-  )
-
-  private def formattingPreferences =
-    FormattingPreferences()
-      .setPreference(AlignParameters, false)
-      .setPreference(AlignSingleLineCaseStatements, true)
-      .setPreference(AlignSingleLineCaseStatements.MaxArrowIndent, 90)
-      .setPreference(DanglingCloseParenthesis, Preserve)
-      .setPreference(DoubleIndentClassDeclaration, true)
-      .setPreference(RewriteArrowSymbols, true)
+  val formattingSettings = Seq(ScalariformKeys.preferences := ScalariformKeys.preferences.value
+    .setPreference(AlignParameters, false)
+    .setPreference(AlignSingleLineCaseStatements, true)
+    .setPreference(AlignSingleLineCaseStatements.MaxArrowIndent, 90)
+    .setPreference(DanglingCloseParenthesis, Preserve)
+    .setPreference(DoubleIndentConstructorArguments, true)
+    .setPreference(RewriteArrowSymbols, true))
 }
--- project/Publishing.scala
+++ project/Publishing.scala
@@ -15,30 +15,30 @@ object Publishing {
       if (isSnapshot.value)
         Some("snapshots" at nexus + "content/repositories/snapshots")
       else
-        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+        Some("releases" at nexus + "service/local/staging/deploy/maven2")
     },
     credentials ++= (
       for {
-        username <- sys.env.get("SONATYPE_USERNAME")
-        password <- sys.env.get("SONATYPE_PASSWORD")
+        username ← sys.env.get("SONATYPE_USERNAME")
+        password ← sys.env.get("SONATYPE_PASSWORD")
       } yield Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", username, password)).toSeq,
     pomExtra := (
-  <url>https://github.com/xebia/cakemix#readme</url>
-  <scm>
-    <url>git@github.com:xebia/cakemix.git</url>
-    <connection>scm:git@github.com:xebia/cakemix.git</connection>
-  </scm>
-  <developers>
-    <developer>
-      <id>agemooij</id>
-      <name>Age Mooij</name>
-      <url>http://github.com/agemooij</url>
-    </developer>
-    <developer>
-      <id>raboof</id>
-      <name>Arnout Engelen</name>
-      <url>http://github.com/raboof</url>
-    </developer>
-  </developers>
+      <url>https://github.com/xebia/cakemix#readme</url>
+      <scm>
+        <url>git@github.com:xebia/cakemix.git</url>
+        <connection>scm:git@github.com:xebia/cakemix.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>agemooij</id>
+          <name>Age Mooij</name>
+          <url>http://github.com/agemooij</url>
+        </developer>
+        <developer>
+          <id>raboof</id>
+          <name>Arnout Engelen</name>
+          <url>http://github.com/raboof</url>
+        </developer>
+      </developers>
     ))
 }
