19eb2a64fd97095cd88266935117f5024bf8616a
Philippus <philippus@gmail.com>
2019-03-29 14:40:24 +0100

    Relicense under Apache 2.0

--- src/main/scala/cakemix/SchedulerProvider.scala
+++ src/main/scala/cakemix/SchedulerProvider.scala
@@ -1,3 +1,15 @@
+/*
+ * Cakemix
+ *
+ * Copyright Xebia BV and Wehkamp BV
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
 package cakemix
 
 import akka.actor.{ Actor, Scheduler }
